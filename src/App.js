81ce9f6b5a7c2066fee66aace420e3504007df29
DungNg <87799163+nminhdung@users.noreply.github.com>
2023-04-11 11:28:40 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import ImageSlider from "./components/ImageSlider";
 import Navbar from "./components/Navbar";
 import Offer from "./components/Offer";
 import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Hero />
       <Offer/>
       <Plan/>
+      <Rooms/>
+      <ImageSlider/>
+      <Footer/>
     </div>
   );
 }
